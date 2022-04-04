cf5c2a3acb5880dcb9b890ffad13770bd43d6105
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-04 16:59:55 -0300

    Add files via upload

---
+++ Diference/Diference.js
@@ -0,0 +1,21 @@
+import { print, input } from "./io_utils.js";
+
+function main() {
+    const A = Number(input('A:'))
+    const B = Number(input('B:'))
+    const C = Number(input('C:'))
+    const D = Number(input('D:'))
+    const expression = (A * B) - (C * D)
+
+
+
+
+
+    print(`The expression is ${expression}`)
+
+
+
+
+
+}
+main()
