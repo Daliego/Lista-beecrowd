99aec301bd2638c1923f3463a60c6221c125bc55
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-04 16:36:28 -0300

    Add files via upload

---
+++ Simple_product/Simple_product.js
@@ -0,0 +1,19 @@
+import { print, input } from "./io_utils.js";
+
+function main() {
+    const A = Number(input('Digite o numero 1:'))
+    const B = Number(input('Digite o numero 2:'))
+    const product = A * B
+
+
+
+
+
+    print(`The product is ${product}`)
+
+
+
+
+
+}
+main()
