8873da0c1bba3d7c7df0885c16d8d5960e0e99bf
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-04 17:09:40 -0300

    Add files via upload

---
+++ Salary/Salary.js
@@ -0,0 +1,21 @@
+import { print, input } from "./io_utils.js";
+
+function main() {
+    const Worked_hours = Number(input('Workedhours:'))
+    const Employee_number = Number(input('Emṕloyee number:'))
+    const Money_hour = Number(input('Money/hour is:'))
+
+    const salary = Worked_hours * Money_hour
+
+
+
+
+
+    print(`The employee number is ${Employee_number} and the salary is ${salary} reals`)
+
+
+
+
+
+}
+main()
