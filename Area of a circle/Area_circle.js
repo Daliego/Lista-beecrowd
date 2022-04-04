c4630a273f35982ca42db010a7f5970871f88349
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-04 16:31:40 -0300

    Add files via upload

---
+++ Area of a circle/Area_circle.js
@@ -0,0 +1,20 @@
+import { print, input } from "./io_utils.js";
+
+function main() {
+    const raio = Number(input('Digite o raio do círculo:'))
+    const pi = 3.14159
+
+    const area_circle = pi * (raio ** 2)
+
+
+
+
+
+    print(`A area é ${area_circle.toFixed(2)}`)
+
+
+
+
+
+}
+main()
