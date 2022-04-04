e8a4ca29a2c4ec1f2e63306d51b62e79d14b868c
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-04 16:34:08 -0300

    Add files via upload

---
+++ Simple_sum/Simple_sum.js
@@ -0,0 +1,19 @@
+import { print, input } from "./io_utils.js";
+
+function main() {
+    const A = Number(input('Digite o numero 1:'))
+    const B = Number(input('Digite o numero 2:'))
+    const soma = A + B
+
+
+
+
+
+    print(`A soma é ${soma}`)
+
+
+
+
+
+}
+main()
